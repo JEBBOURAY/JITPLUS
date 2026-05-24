49afeea153786eb38699ef282a4aea65e6276789
ayoub <ayoub.je97@gmail.com>
2026-05-24 23:02:36 +0100

    chore(jitplus): bump version to 1.3.17

--- apps/jitplus/app.config.js
+++ apps/jitplus/app.config.js
@@ -36,7 +36,7 @@ module.exports = ({ config }) => {
     name: 'JitPlus',
     slug: 'jitplus',
     description: 'Digital loyalty cards app — collect stamps and earn rewards at your favorite local shops.',
-    version: '1.3.15',
+    version: '1.3.17',
     orientation: 'default',
     icon: './assets/images/icon-white.png',
     scheme: 'jitplus',
